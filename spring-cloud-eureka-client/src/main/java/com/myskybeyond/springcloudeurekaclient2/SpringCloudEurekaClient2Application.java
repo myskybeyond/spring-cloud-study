5d6abede9f92c76ef5e62157d5cf1b5abe343b66
myskybeyond <37172114+myskybeyond@users.noreply.github.com>
2021-04-29 13:46:25 +0800

    Add files via upload

---
+++ spring-cloud-eureka-client/src/main/java/com/myskybeyond/springcloudeurekaclient2/SpringCloudEurekaClient2Application.java
@@ -0,0 +1,25 @@
+package com.myskybeyond.springcloudeurekaclient2;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+//@EnableDiscoveryClient
+@RestController
+public class SpringCloudEurekaClient2Application {
+
+    @Value("${server.port}")
+    String port;
+    @RequestMapping("/")
+    public String home() {
+        return "Hello world ,port:" + port;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaClient2Application.class, args);
+    }
+
+}
